58b607bdab3829fc7e21c18d5ba682b722ed9ebb
AmrNabel <121950000+AmrNabel@users.noreply.github.com>
2024-04-30 04:41:03 +0300

    text editpr ui implementing

--- app/components/Texteditor.tsx
+++ app/components/Texteditor.tsx
@@ -1,43 +1,87 @@
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import { Box, FormLabel} from '@mui/material';
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { Box, FormLabel } from "@mui/material";
+import "./TextEditor.css";
 
 function TextEditor() {
-    const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-    const handleChange = (content: any, delta: any, source: any, editor: { getHTML: () => React.SetStateAction<string>; }) => {
-        setText(editor.getHTML());
-    };
+  const handleChange = (
+    content: any,
+    delta: any,
+    source: any,
+    editor: { getHTML: () => React.SetStateAction<string> }
+  ) => {
+    setText(editor.getHTML());
+  };
 
-    const handleSave = () => {
-        console.log("Saving data:", text);
-    };
+  const handleSave = () => {
+    console.log("Saving data:", text);
+  };
 
-    const handleBlur = () => {
-        handleSave();
-    };
+  const handleBlur = () => {
+    handleSave();
+  };
 
-    return (
-        <Box sx={{ m: 'auto',width:'100%',borderRadius: "8px", ml: "-10px"}}>
-            <FormLabel
-                  sx={{
-                    color: "#202E5B",
-                    fontFamily: "Lato",
-                    fontSize: "16px",
-                    fontWeight: 600,
-                    "&.Mui-focused": {
-                      color: "#000",
-                    },
-                  }}
-                  id="name-label"
-                >
-                  Credential Name
-                  <span style={{ color: "red" }}> *</span>
-                </FormLabel>
-            <ReactQuill style={{marginTop:'5px'}} theme="snow" value={text} onChange={handleChange} onBlur={handleBlur} />
-        </Box>
-    );
+  const modules = {
+    toolbar: [
+      [
+        "bold",
+        "italic",
+        "underline",
+        "strike",
+        "link",
+        { list: "ordered" },
+        { list: "bullet" },
+        { list: "check" },
+        "code-block",
+      ],
+    ],
+  };
+
+  const formats = [
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "code-block",
+    "bullet",
+    "link",
+    'ordered',
+    'check',
+  ];
+
+  return (
+    <Box sx={{ width: "103%", borderRadius: "8px", ml: "-10px" }}>
+      <FormLabel
+        sx={{
+          color: "#202E5B",
+          fontFamily: "Lato",
+          fontSize: "16px",
+          fontWeight: 600,
+          "&.Mui-focused": {
+            color: "#000",
+          },
+        }}
+        id="name-label"
+      >
+        Earning Criteria
+        <span style={{ color: "red" }}> *</span>
+      </FormLabel>
+      <ReactQuill
+        theme="snow"
+        value={text}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        modules={modules}
+        formats={formats}
+        style={{ marginTop: "4px" }}
+        placeholder="e.g., Managed a local garden for 2 years, Organized weekly gardening workshops, Led a community clean-up initiative"
+      />
+    </Box>
+  );
 }
 
 export default TextEditor;
