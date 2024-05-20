2b2b12e68271480a5830ef466a5b1878dfb44042
AmrNabel <121950000+AmrNabel@users.noreply.github.com>
2024-05-20 15:58:44 +0300

    custom theme structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,69 @@ const inter = Inter({ subsets: ["latin"] });
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#009688",
+      main: "#003FE0",
     },
     secondary: {
       main: "#FFFFFF",
+    },
+  },
+  typography: {
+    fontFamily: "Lato, Roboto, Inter, Poppins",
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: "#202E5B",
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "capitalize",
+          borderRadius: "100px",
+          fontFamily: "Roboto",
+          lineHeight: "20px",
+          padding: "10px 24px",
+        },
+        containedPrimary: {
+          backgroundColor: "#003FE0",
+          color: "#FFF",
+          "&:hover": {
+            backgroundColor: "#003FE0",
+          },
+        },
+        containedSecondary: {
+          backgroundColor: "#FFF",
+          color: "#4E4E4E",
+          "&:hover": {
+            backgroundColor: "#FFF",
+          },
+        },
+      },
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          color: "var(--T3-Body-Text, #202E5B)",
+          fontSize: "16px",
+          fontWeight: 600,
+          "&.Mui-focused": {
+            color: "#000",
+          },
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#FFF",
+          "& .MuiOutlinedInput-root": {
+            borderRadius: "8px",
+          },
+        },
+      },
     },
   },
   breakpoints: {
