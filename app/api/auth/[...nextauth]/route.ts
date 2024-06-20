c5bcda8b6b9f0d81eb143992744bbd16bb9b1418
AmrNabel <121950000+AmrNabel@users.noreply.github.com>
2024-06-20 18:42:38 +0300

    manage  the scop and save to drive
    Former-commit-id: faed0c0cad97bd2e3cecaa4c46189c88de8cbacb

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -14,7 +14,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
       authorization: {
         params: {
-          scope: "https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/drive.appfolder",
+          scope: 'openid email profile https://www.googleapis.com/auth/drive',
         },
       },
     }),
@@ -23,11 +23,13 @@ const handler = NextAuth({
     async jwt({ token, account }) {
       if (account) {
         token.accessToken = account.access_token;
+        token.idToken = account.id_token;
       }
       return token;
     },
     async session({ session, token }) {
       session.accessToken = token.accessToken as string;
+      session.idToken = token.idToken as string;
       return session;
     },
   },
--- app/components/SigninButton.tsx
+++ app/components/SigninButton.tsx
@@ -1,58 +1,57 @@
-"use client";
-import React, { useEffect } from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Box, Button, Typography } from "@mui/material";
-import { DefaultSession } from "next-auth";
+'use client'
+import React, { useEffect } from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { Box, Button, Typography } from '@mui/material'
+import { DefaultSession } from 'next-auth'
 
 interface ExtendedSession extends DefaultSession {
-  accessToken?: string;
+  accessToken?: string
+  idToken?: string
 }
 
 const buttonStyle = {
-  padding: "10px 20px",
-  borderRadius: "100px",
-  textTransform: "capitalize",
-  fontFamily: "Roboto",
-  fontWeight: "600",
-  lineHeight: "20px",
-  border: "1px solid  #4E4E4E",
-  backgroundColor: "#003FE0",
-  color: "#FFF",
-  "&:hover": {
-    backgroundColor: "#003FE0",
-  },
-};
+  padding: '10px 20px',
+  borderRadius: '100px',
+  textTransform: 'capitalize',
+  fontFamily: 'Roboto',
+  fontWeight: '600',
+  lineHeight: '20px',
+  border: '1px solid  #4E4E4E',
+  backgroundColor: '#003FE0',
+  color: '#FFF',
+  '&:hover': {
+    backgroundColor: '#003FE0'
+  }
+}
 
 const SigninButton = () => {
-  const { data: session } = useSession();
-  console.log(":  SigninButton  data", session)
+  const { data: session } = useSession()
+  console.log('SigninButton data:', session)
 
   useEffect(() => {
-    const extendedSession = session as ExtendedSession;
+    const extendedSession = session as ExtendedSession
     if (extendedSession?.accessToken) {
-      localStorage.setItem('accessToken', extendedSession.accessToken);
+      localStorage.setItem('accessToken', extendedSession.accessToken)
     } else {
-      localStorage.removeItem('accessToken');
+      localStorage.removeItem('accessToken')
     }
-  }, [session]);
+  }, [session])
 
   if (session && session.user) {
     return (
-      <Box
-        sx={{ display: "flex", alignItems: "center", gap: "20px", mt: "20px" }}
-      >
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: '20px', mt: '20px' }}>
         <Typography>Hi, {session.user.name}</Typography>
         <Button sx={buttonStyle} onClick={() => signOut()}>
           Sign Out
         </Button>
       </Box>
-    );
+    )
   }
   return (
-    <Button sx={{ ...buttonStyle, mt: "20px" }} onClick={() => signIn()}>
+    <Button sx={{ ...buttonStyle, mt: '20px' }} onClick={() => signIn()}>
       Sign In
     </Button>
-  );
-};
+  )
+}
 
-export default SigninButton;
+export default SigninButton
