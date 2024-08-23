da0da43f364f6903c92be6a633d0fcafd37f8992
Omar Salah <omar.salah1597@gmail.com>
2024-08-23 19:09:48 +0300

    fix typo
    Former-commit-id: 7631d0f6707bc9b73002fcdc610595c24852ea69

--- app/Recommendations/[credntialData]/RecommandationForm/Steps/SuccessPage.tsx
+++ app/Recommendations/[credntialData]/RecommandationForm/Steps/SuccessPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, Typography, Button } from '@mui/material'
 import Image from 'next/image'
-import BadgeIcon from '../../../../Assets/SVGs/badge.svg'
+import BadgeIcon from '../../../../Assets/SVGs/Badge.svg'
 
 const SuccessPage = () => {
   return (
