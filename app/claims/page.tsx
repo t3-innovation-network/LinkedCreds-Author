d86fc8650aecc1e6135160b9b955c26423cb5938
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2024-10-22 06:06:28 +0300

    add the loading to claims route
    Former-commit-id: f8aa6ca82c09b99b2ba1f9b15ae325f462997b3d [formerly 2d38d793ee39298aaf8b442c05d6d718acc82d35]
    Former-commit-id: 2c364e04b4df118868f146b9bc24f4da9e1ef9e6

--- app/claims/page.tsx
+++ app/claims/page.tsx
@@ -90,7 +90,6 @@ const ClaimsPage: React.FC = () => {
   }, [accessToken, storage, getAllClaims])
 
   const handleClaimClick = async (claimId: string, content: any) => {
-    console.log(':  handleClaimClick  claimId', claimId)
     try {
       if (openClaim === claimId) {
         setOpenClaim(null)
@@ -115,158 +114,171 @@ const ClaimsPage: React.FC = () => {
         Previous Claims
       </Typography>
       <List>
-        {claims.map(claim => (
-          <div key={claim.id}>
-            <ListItem
-              button
-              onClick={() => handleClaimClick(claim.content.id, claim.content)}
-            >
-              <ListItemText
-                primary={claim.content.credentialSubject?.achievement?.[0]?.name}
-              />
-              {openClaim === claim.content.id ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={openClaim === claim.content.id} timeout='auto' unmountOnExit>
-              <Container>
-                {loadingClaims[claim.content.id] ? (
-                  <CircularProgress />
-                ) : (
-                  <Box>
-                    {/* Claim Details Box */}
-                    <Box
-                      sx={{
-                        border: '1px solid #003FE0',
-                        borderRadius: '10px',
-                        p: '15px',
-                        mb: '10px',
-                        bgcolor: '#d5e1fb'
-                      }}
-                    >
+        {claims.length !== 0 ? (
+          claims.map(claim => (
+            <div key={claim.id}>
+              <ListItem
+                button
+                onClick={() => handleClaimClick(claim.content.id, claim.content)}
+              >
+                <ListItemText
+                  primary={claim.content.credentialSubject?.achievement?.[0]?.name}
+                />
+                {openClaim === claim.content.id ? <ExpandLess /> : <ExpandMore />}
+              </ListItem>
+              <Collapse in={openClaim === claim.content.id} timeout='auto' unmountOnExit>
+                <Container>
+                  {loadingClaims[claim.content.id] ? (
+                    <CircularProgress />
+                  ) : (
+                    <Box>
+                      {/* Claim Details Box */}
                       <Box
                         sx={{
-                          display: 'flex',
-                          gap: '5px',
-                          alignItems: 'center'
+                          border: '1px solid #003FE0',
+                          borderRadius: '10px',
+                          p: '15px',
+                          mb: '10px',
+                          bgcolor: '#d5e1fb'
                         }}
                       >
-                        <SVGBadge />
-                        <Typography
-                          sx={{ fontWeight: 700, fontSize: '13px', color: '#202E5B' }}
-                        >
-                          {detailedClaim?.credentialSubject?.name ?? ''} has claimed:
-                        </Typography>
-                      </Box>
-                      <Typography
-                        sx={{
-                          color: '#202E5B',
-                          fontFamily: 'Inter',
-                          fontSize: '24px',
-                          fontWeight: 700,
-                          letterSpacing: '0.075px',
-                          mb: '10px'
-                        }}
-                      >
-                        {claim.achievementName}
-                      </Typography>
-                      {detailedClaim?.credentialSubject?.duration && (
                         <Box
                           sx={{
-                            ...credentialBoxStyles,
-                            bgcolor: '#f9f9f9'
+                            display: 'flex',
+                            gap: '5px',
+                            alignItems: 'center'
                           }}
                         >
-                          <Box sx={{ mt: '2px' }}>
-                            <SVGDate />
-                          </Box>
+                          <SVGBadge />
                           <Typography
-                            sx={{ ...commonTypographyStyles, fontSize: '13px' }}
+                            sx={{ fontWeight: 700, fontSize: '13px', color: '#202E5B' }}
                           >
-                            {detailedClaim?.credentialSubject?.duration}
+                            {detailedClaim?.credentialSubject?.name ?? ''} has claimed:
                           </Typography>
                         </Box>
-                      )}
-                      <Typography
-                        sx={{
-                          fontFamily: 'Lato',
-                          fontSize: '17px',
-                          letterSpacing: '0.075px',
-                          lineHeight: '24px'
-                        }}
-                      >
-                        {detailedClaim?.credentialSubject?.achievement[0]?.description.replace(
-                          /<\/?[^>]+>/gi,
-                          ''
+                        <Typography
+                          sx={{
+                            color: '#202E5B',
+                            fontFamily: 'Inter',
+                            fontSize: '24px',
+                            fontWeight: 700,
+                            letterSpacing: '0.075px',
+                            mb: '10px'
+                          }}
+                        >
+                          {claim.achievementName}
+                        </Typography>
+                        {detailedClaim?.credentialSubject?.duration && (
+                          <Box
+                            sx={{
+                              ...credentialBoxStyles,
+                              bgcolor: '#f9f9f9'
+                            }}
+                          >
+                            <Box sx={{ mt: '2px' }}>
+                              <SVGDate />
+                            </Box>
+                            <Typography
+                              sx={{ ...commonTypographyStyles, fontSize: '13px' }}
+                            >
+                              {detailedClaim?.credentialSubject?.duration}
+                            </Typography>
+                          </Box>
                         )}
-                      </Typography>
-                      {detailedClaim?.credentialSubject?.achievement[0]?.criteria
-                        ?.narrative && (
-                        <Box>
-                          <Typography>Earning criteria:</Typography>
-                          <ul style={{ marginLeft: '25px' }}>
-                            <li>
-                              {detailedClaim?.credentialSubject?.achievement[0]?.criteria?.narrative.replace(
-                                /<\/?[^>]+>/gi,
-                                ''
+                        <Typography
+                          sx={{
+                            fontFamily: 'Lato',
+                            fontSize: '17px',
+                            letterSpacing: '0.075px',
+                            lineHeight: '24px'
+                          }}
+                        >
+                          {detailedClaim?.credentialSubject?.achievement[0]?.description.replace(
+                            /<\/?[^>]+>/gi,
+                            ''
+                          )}
+                        </Typography>
+                        {detailedClaim?.credentialSubject?.achievement[0]?.criteria
+                          ?.narrative && (
+                          <Box>
+                            <Typography>Earning criteria:</Typography>
+                            <ul style={{ marginLeft: '25px' }}>
+                              <li>
+                                {detailedClaim?.credentialSubject?.achievement[0]?.criteria?.narrative.replace(
+                                  /<\/?[^>]+>/gi,
+                                  ''
+                                )}
+                              </li>
+                            </ul>
+                          </Box>
+                        )}
+                        {detailedClaim?.credentialSubject?.portfolio && (
+                          <Box>
+                            <Typography>Supporting Evidence:</Typography>
+                            <ul style={evidenceListStyles}>
+                              {detailedClaim?.credentialSubject?.portfolio?.map(
+                                (porto: { url: any; name: any }) => (
+                                  <li key={porto.url}>
+                                    <Link
+                                      href={porto.url}
+                                      target='_blank'
+                                      rel='noopener noreferrer'
+                                    >
+                                      {porto.name}
+                                    </Link>
+                                  </li>
+                                )
                               )}
-                            </li>
-                          </ul>
+                            </ul>
+                          </Box>
+                        )}
+                        <Box
+                          sx={{ mt: 2, display: 'flex', justifyContent: 'space-between' }}
+                        >
+                          <Link href={`/view/${claim.content.id}`}>
+                            <Button
+                              variant='contained'
+                              sx={{
+                                backgroundColor: '#003FE0',
+                                textTransform: 'none',
+                                borderRadius: '100px'
+                              }}
+                            >
+                              View Credential
+                            </Button>
+                          </Link>
+                          <Link href={`/askforrecommendation/${claim.content.id}`}>
+                            <Button
+                              variant='contained'
+                              sx={{
+                                backgroundColor: '#003FE0',
+                                textTransform: 'none',
+                                borderRadius: '100px'
+                              }}
+                            >
+                              Ask for Recommendation
+                            </Button>
+                          </Link>
                         </Box>
-                      )}
-                      {detailedClaim?.credentialSubject?.portfolio && (
-                        <Box>
-                          <Typography>Supporting Evidence:</Typography>
-                          <ul style={evidenceListStyles}>
-                            {detailedClaim?.credentialSubject?.portfolio?.map(
-                              (porto: { url: any; name: any }) => (
-                                <li key={porto.url}>
-                                  <Link
-                                    href={porto.url}
-                                    target='_blank'
-                                    rel='noopener noreferrer'
-                                  >
-                                    {porto.name}
-                                  </Link>
-                                </li>
-                              )
-                            )}
-                          </ul>
-                        </Box>
-                      )}
-                      <Box
-                        sx={{ mt: 2, display: 'flex', justifyContent: 'space-between' }}
-                      >
-                        <Link href={`/view/${claim.content.id}`}>
-                          <Button
-                            variant='contained'
-                            sx={{
-                              backgroundColor: '#003FE0',
-                              textTransform: 'none',
-                              borderRadius: '100px'
-                            }}
-                          >
-                            View Credential
-                          </Button>
-                        </Link>
-                        <Link href={`/askforrecommendation/${claim.content.id}`}>
-                          <Button
-                            variant='contained'
-                            sx={{
-                              backgroundColor: '#003FE0',
-                              textTransform: 'none',
-                              borderRadius: '100px'
-                            }}
-                          >
-                            Ask for Recommendation
-                          </Button>
-                        </Link>
                       </Box>
                     </Box>
-                  </Box>
-                )}
-              </Container>
-            </Collapse>
-          </div>
-        ))}
+                  )}
+                </Container>
+              </Collapse>
+            </div>
+          ))
+        ) : (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              minHeight: '100%'
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        )}
       </List>
     </Container>
   )
