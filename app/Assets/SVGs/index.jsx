0bf6e75f6c438cc5111bb005189c9cf33871c363
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2024-10-23 20:50:42 +0300

    handle humborger icon, sign in and sign out
    Former-commit-id: 89f8af1544d1139639d41b867e9107bd7e017009 [formerly 616a323338d16b01f451367481e94b3367b57bf5]
    Former-commit-id: 42ffa614f6e1438fa9a3e0ff62133f42c2ce532f

--- app/Assets/SVGs/index.jsx
+++ app/Assets/SVGs/index.jsx
@@ -674,6 +674,23 @@ export const LineSVG = () => {
     </Box>
   )
 }
+export const HumborgerMenuSVG = () => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+      >
+        <path d='M4 12H20' stroke='black' strokeLinecap='round' strokeLinejoin='round' />
+        <path d='M4 6H20' stroke='black' strokeLinecap='round' strokeLinejoin='round' />
+        <path d='M4 18H20' stroke='black' strokeLinecap='round' strokeLinejoin='round' />
+      </svg>
+    </Box>
+  )
+}
 export const CopySVG = () => {
   return (
     <Box sx={{ display: 'flex', alignItems: 'center' }}>
