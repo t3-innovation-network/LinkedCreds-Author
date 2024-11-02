663e86f36ed7f960c1c12ee0afd1e4b3d4f79ca4
Golda Velez <gvelez17@gmail.com>
2024-11-02 11:21:01 -0700

    signandSave utility
    Former-commit-id: 39664b1e9e60806ecda4e242e6e0bca03a4eed12 [formerly b729bd3467340dc02e00302fef01c841ffb28606]
    Former-commit-id: 1605c510b1a18b9c7895f21c1592baf0e43ad110

--- app/utils/googleDrive.ts
+++ app/utils/googleDrive.ts
@@ -2,12 +2,6 @@ import { GoogleDriveStorage, saveToGoogleDrive } from '@cooperation/vc-storage'
 import { createDID, signCred } from './signCred'
 
 
-export async function getAllClaims(storage: GoogleDriveStorage): Promise<any> {
-  const claimsData = await storage.getAllFilesByType('VCs');
-  if (!claimsData?.length) return [];
-  return claimsData;
-}
-
 
 export async function saveRaw(accessToken: string | undefined, data: any) {
   if (!accessToken) {
@@ -25,6 +19,38 @@ export async function saveRaw(accessToken: string | undefined, data: any) {
     throw error
   }
 }
+
+export async function signAndSave(accessToken: string | undefined, data: any) {
+  if (!accessToken) {
+    throw new Error('No access token available')
+  }
+
+  const storage = new GoogleDriveStorage(accessToken)
+  try {
+    const { didDocument, keyPair, issuerId } = await createDID(accessToken)
+    const saveResponse = await saveToGoogleDrive(
+      storage,
+      {
+        didDocument,
+        keyPair
+      },
+      'DID'
+    )
+    const res = await signCred(accessToken, data, issuerId, keyPair, 'VC')
+    const file = (await saveToGoogleDrive(storage, res, 'VC')) as any
+
+    console.log('saved to google drive:', res)
+    return file
+  } catch (error) {
+    console.error('Google Drive operation failed:', error)
+    throw error
+  }
+}
+
+
+
+
+
 
 export function makeGoogleDriveLink(file: any) {
    return `https://drive.google.com/file/d/${file.id}/view`
