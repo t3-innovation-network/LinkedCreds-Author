9c3bb9c29513d53b5c3f1411324ccf11fe8b0f1b
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2024-11-05 13:41:52 +0200

    fix navbar
    Former-commit-id: 6fad6565ae2d9a4f74306df8df75e21a931c9935 [formerly c4bc795d3df2e932965271e89cbfd6893ca90a1d]
    Former-commit-id: bd966fcf32177cd960b365ab7103c50bb9bc816e

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -63,7 +63,7 @@ const NavBar = () => {
           width: '55%',
           display: { xs: 'none', md: 'flex' },
           alignItems: 'center',
-          justifyContent: 'space-between',
+          justifyContent: session ? 'space-between' : 'space-between',
           mr: { xs: '15px', md: '10.938vw' }
         }}
       >
