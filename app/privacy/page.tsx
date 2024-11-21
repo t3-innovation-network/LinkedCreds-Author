310f1abb630348d620029d7e8f96b24f5d1d07ff
Muhammad Hany <dev.mhany@gmail.com>
2024-11-21 22:51:50 +0200

    PrivacyPolicy 21Nov Edit
    Former-commit-id: 3b366adb321cc2cb5dc1ef9e3513bab5aed2af60 [formerly 9e735cf2731cf79a0264656f9325b30fc76861c9]
    Former-commit-id: fcff3b1b25613047ab89102119ef5541e1bb3450

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 import { Box, Typography, Link } from '@mui/material'
 import React from 'react'
+import { Logo } from '../Assets/SVGs'
+import { useTheme } from '@mui/material/styles'
 
 const PrivacyPolicy = () => {
+  const theme = useTheme()
   return (
     <Box
       sx={{
@@ -16,6 +19,32 @@ const PrivacyPolicy = () => {
         margin: 'auto'
       }}
     >
+      <Box
+        sx={{
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          ml: { xs: '15px', md: '12.5vw' }
+        }}
+      >
+        <Link href='/' aria-label='OpenCreds Home'>
+          <Box sx={{ display: 'flex', alignItems: 'center', marginRight: 1 }}>
+            <Logo />
+          </Box>
+        </Link>
+        <Link href='/' aria-label='OpenCreds Home'>
+          <Typography
+            sx={{
+              fontWeight: '700',
+              fontSize: { xs: '18px', md: '24px' },
+              color: theme.palette.t3DarkSlateBlue
+            }}
+          >
+            OpenCreds
+          </Typography>
+        </Link>
+      </Box>
+
       <Typography variant='h4' sx={{ fontWeight: 700, mb: 2 }}>
         OpenCreds Privacy Policy
       </Typography>
@@ -39,22 +68,57 @@ const PrivacyPolicy = () => {
         What Personal Information We Collect
       </Typography>
       <Typography sx={{ fontSize: '16px', lineHeight: '1.6' }}>
-        We do not collect any personal information. Any data or documents created by this
-        application are kept solely in storage under the user&apos;s control, such as
-        their own Google Drives.
+        No personal information is collected.
+        Any data or documents created by this application are kept solely in storage
+        under the user&apos;s control, on their own Google Drives.
       </Typography>
 
-      <Typography sx={{ fontWeight: 600, mt: 2 }}>Additional Information</Typography>
+      <Typography sx={{ fontWeight: 600, mt: 2 }}>
+        Google User Data Usage and Protection
+      </Typography>
+      <Typography sx={{ fontWeight: 600, mt: 2 }}>
+        Data Usage
+      </Typography>
       <Typography sx={{ fontSize: '16px', lineHeight: '1.6' }}>
-        We may change this Privacy Policy from time to time. If we make any significant
-        changes in the way we treat your personal information, we will make this clear on
-        our website or by contacting you directly.
+        - Google Drive storage is accessed only to save user authored credentials as JSON-LD documents. No user data is stored, processed, or retained by the application.
+        <br />
+        - All data interactions occur directly between the user&apos browser and Google&apos;s services.
+        <br />
+        - The application cannot access the user&apos;s Google data without explicit user authorization.
       </Typography>
 
-      <Typography sx={{ fontSize: '16px', lineHeight: '1.6', mt: 2 }}>
-        The controller for your personal information is the Learner Credential Wallet
-        project at MIT. We can be contacted at{' '}
-        <Link href='mailto:support@opencreds.net'>support@opencreds.net</Link>.
+      <Typography sx={{ fontWeight: 600, mt: 2 }}>
+        Data Sharing and Transfer
+      </Typography>
+      <Typography sx={{ fontSize: '16px', lineHeight: '1.6' }}>
+        - User data is never shared to any third party from Google Drive or the app itself.
+        <br />
+        - No user data stored on Google Drive is sold or otherwise monetized.
+        <br />
+        - The application does not retain any user data at the conclusion of the session.
+      </Typography>
+
+      <Typography sx={{ fontWeight: 600, mt: 2 }}>
+        Data Protection
+      </Typography>
+      <Typography sx={{ fontSize: '16px', lineHeight: '1.6' }}>
+        - All data transmissions use encrypted HTTPS, secure socket layer connections.
+        <br />
+        - OAuth 2.0 security protocols are used for Google authentication.
+        <br />
+        - Access tokens are stored securely in the browser session.
+        <br />
+        - Strict access controls and security monitoring are followed.
+        <br />
+        - Security measures are regularly monitored and updated to protect against vulnerabilities.
+      </Typography>
+
+      <Typography sx={{ fontWeight: 600, mt: 2 }}>
+        Additional Information
+      </Typography>
+      <Typography sx={{ fontSize: '16px', lineHeight: '1.6' }}>
+        This Privacy Policy may change from time to time. Any significant changes in the way
+        personal information is handled will be clearly described in the Privacy Policy.
       </Typography>
 
       <Typography
