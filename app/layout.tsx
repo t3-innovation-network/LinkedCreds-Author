ac094a43e55cdc07b1b5505251722fb9452bdb54
Muhammad Hany <dev.mhany@gmail.com>
2024-12-09 13:43:47 +0200

    ✨ feat: The appropriate metadata is added.
    Former-commit-id: c79c453374083c8f0d2a0451505f70fe5d5b4bc0 [formerly 36dcf5261a079d0a7a599f819b56ba0d07e6ac51]
    Former-commit-id: 9b37f83e876b7d77a16c801c8d88711187629930

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,35 @@ import background from './Assets/Images/Background.svg' // Import background ima
 
 const inter = Inter({ subsets: ['latin'] })
 
+const structuredData = {
+  '@context': 'https://schema.org',
+  '@type': 'WebApplication',
+  name: 'OpenCreds',
+  url: 'https://opencreds.net',
+  applicationCategory: 'CredentialManagement',
+  operatingSystem: 'Web-based',
+  description:
+    'OpenCreds is a secure and efficient credential management platform. Easily capture, store, and share your professional credentials to unlock new opportunities.',
+  featureList: [
+    'Credential Capture',
+    'Secure Storage',
+    'Easy Sharing',
+    'Recommendation Requests',
+    'Integration with Professional Networks'
+  ],
+  sameAs: [
+    'https://github.com/Cooperation-org',
+    'https://www.linkedin.com/company/linkedtrust/'
+  ],
+  publisher: {
+    '@type': 'Organization',
+    name: 'OpenCreds',
+    url: 'https://opencreds.net',
+    logo: 'https://opencreds.net/images/logo.png'
+  },
+  applicationSubCategory: 'Credential Management'
+}
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -25,8 +54,76 @@ export default function RootLayout({
       <CssBaseline />
       <html lang='en'>
         <Head>
+          {/* Inject structured data */}
+          <script
+            type='application/ld+json'
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+          />
+          {/* Metadata */}
           <title>OpenCreds</title>
           <script src='https://accounts.google.com/gsi/client' async defer></script>
+          <meta
+            name='description'
+            content='OpenCreds offers the ultimate solution for managing your professional credentials. Capture, store, and share your achievements securely to unlock new opportunities.'
+          />
+          {/* add the content when applying in google search https://search.google.com/search-console/welcome */}
+          {/* <meta name='google-site-verification' content='' /> */}
+
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <meta
+            name='keywords'
+            content='Credential Management, Professional Credentials, Secure Storage, Share Achievements, OpenCreds'
+          />
+          <meta name='author' content='Linked Trust' />
+
+          {/* Open Graph */}
+          <meta property='og:title' content='OpenCreds' />
+          <meta
+            property='og:description'
+            content='Manage your professional credentials securely and efficiently with OpenCreds. Capture, store, and share your achievements to unlock new opportunities.'
+          />
+          <meta property='og:url' content='https://opencreds.net' />
+          <meta property='og:site_name' content='OpenCreds' />
+          <meta property='og:image' content='/images/og-image.png' />
+          <meta property='og:image:width' content='1200' />
+          <meta property='og:image:height' content='630' />
+          <meta property='og:type' content='website' />
+
+          {/* Twitter Card */}
+          <meta name='twitter:card' content='summary_large_image' />
+          <meta name='twitter:title' content='OpenCreds' />
+          <meta
+            name='twitter:description'
+            content='Securely manage and share your professional credentials with OpenCreds. Enhance your career opportunities with our comprehensive credential management solution.'
+          />
+          <meta name='twitter:image' content='/images/og-image.png' />
+          <meta name='application-name' content='OpenCreds' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+
+          {/* Preconnect to important third-party domains */}
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
+          {/* Favicon and PWA assets */}
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/icons/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/icons/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/icons/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
         </Head>
         <body
           style={{
