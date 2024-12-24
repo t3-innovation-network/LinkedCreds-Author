af1b4a6ec9f9bb89a4d4f1a589c38aaadf20e11c
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2024-12-24 15:25:36 +0200

    refactor
    Former-commit-id: dd1f5f35ca675532f4497c7caf2a0a93c72e517d [formerly 3a370db96c04ecd7e4b5d7e09640dfd108105cfd]
    Former-commit-id: e003c9a56c007a05820b5fb9c62f096367f96831

--- app/claims/page.tsx
+++ app/claims/page.tsx
@@ -40,32 +40,6 @@ import {
   BlueBadge
 } from '../Assets/SVGs'
 
-// Types
-interface Claim {
-  [x: string]: any
-  id: string
-  achievementName: string
-}
-
-interface ClaimDetail {
-  '@context': string[]
-  id: string
-  type: string[]
-  issuer: {
-    id: string
-    type: string[]
-  }
-  issuanceDate: string
-  expirationDate: string
-  credentialSubject: {
-    type: string[]
-    name: string
-    achievement: any
-    duration: string
-    portfolio: any
-  }
-}
-
 const borderColors = ['#3b82f6', '#8b5cf6', '#ec4899', '#f97316', '#22c55e', '#6366f1']
 
 const getRandomBorderColor = () => {
