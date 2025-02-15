d8425454fc073139c6b18a55dee25d7170fb5243
Amr Nabel <121950000+AmrNabel@users.noreply.github.com>
2025-02-16 01:33:14 +0200

    replace LinkedClaims with LinkedCreds
    Former-commit-id: 42296dcf2e210dcd135d550a3891f9f54820c32a [formerly 8f35e8600195cc3f7eb1bd57095ed72e3658c8ec]
    Former-commit-id: 2743b57042fd5556d36b75145167a4e91d4be2ab

--- app/recommendations/[id]/RecommandationForm/fromTexts/FormTextSteps.tsx
+++ app/recommendations/[id]/RecommandationForm/fromTexts/FormTextSteps.tsx
@@ -5,7 +5,7 @@ import { Box, Typography } from '@mui/material'
 
 export const textGuid = (fullName: string) => [
   <>
-    Hi, I’m Tessa! <br /> Where do you want to save your LinkedClaims?
+    Hi, I’m Tessa! <br /> Where do you want to save your LinkedCreds?
   </>,
   <>
     First, choose where to save your recommendation.{' '}
