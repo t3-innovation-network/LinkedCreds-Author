378d9b338e9ebefe9ec34b2388c2a0afb4b93a2a
Omar Salah <omar.salah1597@gmail.com>
2025-02-27 19:08:56 +0200

    Fix refresh token
    Former-commit-id: e1214b7003021344bd983852124e7d0c2f33e63d [formerly d004e354368a9843c08e55196ff06b37cdaffc80]
    Former-commit-id: b73bb6dc1fc7889839b5a1b6a27435cf9b8eba24

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { setCookie } from '../../../utils/cookie'
-import { storeFileTokens } from '../../../firebase/storage'
-import { refreshAccessToken } from '../../../utils/refreshToken'
 
 declare module 'next-auth' {
   interface Session {
@@ -104,5 +102,46 @@ const handler = NextAuth({
     updateAge: 60 * 60 * 24 // 1 day
   }
 })
+
+async function refreshAccessToken(token: any) {
+  try {
+    const url = 'https://oauth2.googleapis.com/token'
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      method: 'POST',
+      body: new URLSearchParams({
+        client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? '',
+        client_secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? '',
+        grant_type: 'refresh_token',
+        refresh_token: token.refreshToken
+      })
+    })
+
+    const refreshedTokens = await response.json()
+    console.log(':  refreshAccessToken  refreshedTokens', refreshedTokens)
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      expires: Date.now() + refreshedTokens.expires_in * 1000,
+      // If no new refresh token is returned, keep the old one
+      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken
+    }
+  } catch (error) {
+    console.error('Error refreshing access token', error)
+
+    return {
+      ...token,
+      error: 'RefreshAccessTokenError'
+    }
+  }
+}
 
 export { handler as GET, handler as POST }
