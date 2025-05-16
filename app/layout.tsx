334b5a11bd685722305fd69dc2d5a5e1140c9c93
Muhammad Hany <dev.mhany@gmail.com>
2025-05-16 15:55:54 +0300

    test
    Former-commit-id: 90180561e7f68da643b1792eeb5a04114b04f9db [formerly 081f2d5debeaf59486dd847f36d56be46d7ced2b]
    Former-commit-id: 901489e3a18b6cf915ac08260b288a31c2f61731

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next'
 import ClientLayout from './ClientLayout'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+// test
 const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'LinkedCreds',
