b1a0cf45777c16ed452972468d94c1049101dfa9
Muhammad Hany <dev.mhany@gmail.com>
2025-06-24 13:42:21 +0300

    fix build
    Former-commit-id: 007ac5be22973a43fea85b74353ed410641761c4 [formerly b6ce1c1991064991863e7754ee74626445f743e3]
    Former-commit-id: d72e1be16338a253d606680676bedb548d44c83d

--- app/firebase/config/firebase.ts
+++ app/firebase/config/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app'
-import { getFirestore, Firestore } from 'firebase/firestore'
+import { initializeApp, getApps, getApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 import { getAuth, Auth } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -8,20 +8,12 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
-let app: FirebaseApp
-let db: Firestore
-let auth: Auth
-
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig)
-} else {
-  app = getApp()
-}
-
-db = getFirestore(app)
-auth = getAuth(app)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore(app)
+const auth = getAuth(app)
 
 export { app, db, auth, firebaseConfig }
